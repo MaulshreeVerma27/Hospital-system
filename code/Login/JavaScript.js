4354fb38620d1c16eaf4286133a8fc66190705dc
manojreddy1997 <46071984+manojreddy1997@users.noreply.github.com>
2019-09-11 20:32:55 -0500

    Add files via upload

---
+++ code/Login/JavaScript.js
@@ -0,0 +1,12 @@
+﻿
+function validation() {
+    
+    var name = document.getElementById("username").value;
+    var email = document.getElementById("id").value;
+
+    alert(name);
+    var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+
+    document.write(name);
+  
+}
