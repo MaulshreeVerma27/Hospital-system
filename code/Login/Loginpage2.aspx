adee45324481b6e6b32bfb60854bc73fca0b1001
yn0085 <44215351+yn0085@users.noreply.github.com>
2019-10-21 20:06:29 -0500

    Login page

---
+++ code/Login/Loginpage2.aspx
@@ -0,0 +1,97 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Loginpage2.aspx.cs" Inherits="HospitalSystem.Loginpage2" %>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!DOCTYPE html>
+<head>
+    <style>
+        body {
+  background: url(bg2.jpg);
+  background-repeat: no-repeat;
+    background-size: 100% 100%;
+  object-fit:cover;
+  
+}
+        
+
+    </style>
+</head>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <style>
+            .button1 {width: 250px;}
+        </style>
+    </head>
+
+<body >
+     <tr>
+                     
+ 
+
+    <form id="form1" runat="server">
+        <div>
+            <style type="text/css">
+        .auto-style1 {
+            width: 194px;
+        }
+        .auto-style2 {
+            width: 242px;
+        }
+    </style>
+          <table align="center" style="width: 380px" cellspacing="5" cellpadding="7"  >
+  
+                     <tr>
+                     <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="card-header" fon>Login</div>
+                    </tr>
+        <tr>
+            <br><br><br><br><br><br>
+               <td  >&nbsp;</td>
+            <td> Login </td>
+            <td>
+                <asp:TextBox ID="loginTextBox" runat="server" OnTextChanged="loginTextBox_TextChanged"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style1">&nbsp;</td>
+            <td class="auto-style1">Password</td>
+            <td>
+                <asp:TextBox ID="passTextBox" runat="server" OnTextChanged="passTextBox_TextChanged"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style1">&nbsp;</td>
+            <td class="auto-style2">&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style1">&nbsp;</td>
+            <td class="auto-style2">
+                <asp:Button class="btn btn-primary btn-lg btn-block login-button" ID="Button1" runat="server" OnClick="Button1_Click" Text="Login" Width="150px" />
+            </td>
+            <td>
+                <asp:Button class="btn btn-primary btn-lg btn-block login-button" ID="Button2" runat="server" OnClick="Button2_Click" Text="Change password" Width="180px" />
+            </td>
+            <td>
+                <asp:Button class="btn btn-primary btn-lg btn-block login-button" ID="Button3" runat="server" Text="Home" OnClick="Button3_Click" Width="98px" />
+            </td>
+
+        </tr>
+        <tr>
+            <td class="auto-style1">&nbsp;</td>
+            <td class="auto-style2">
+                <asp:Label ID="Label1" runat="server"></asp:Label>
+                </td><td>
+                <asp:Label ID="Label2" runat="server"></asp:Label>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ code/Login/Loginpage2.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HospitalSystem.App_Code;
+
+namespace HospitalSystem
+{
+    public partial class Loginpage2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Class1 c = new Class1();
+            string str = loginTextBox.Text.Trim();
+            Label2.Text = str.Substring(0, 1);
+            if (Equals(str.Substring(0,1),"d")==true)
+                {
+
+                c.cmd.CommandText = "Select * from Doctor1 where DoctorID='" + loginTextBox.Text + "' and pswd='" + passTextBox.Text + "'";
+                c.ds.Clear();
+                c.adp.Fill(c.ds, "vt");
+                if (c.ds.Tables["vt"].Rows.Count == 0)
+                {
+                    Label1.Text = "Username or Password does not exist! Try again";
+                }
+                else
+                {
+                    Session["user"] = loginTextBox.Text;
+                    Response.Redirect("WelcomeDoctor.aspx");
+                }
+                //c.ds.Clear();
+                //c.adp.Fill(c.ds, "vt");
+
+            }
+            else if(Equals(str.Substring(0, 1),"p")==true)
+            {
+                c.cmd.CommandText = "Select * from Patient1 where PatientID='" + loginTextBox.Text + "' and pswd='" + passTextBox.Text + "'";
+                c.ds.Clear();
+                c.adp.Fill(c.ds, "vt");
+                if (c.ds.Tables["vt"].Rows.Count == 0)
+                {
+                    Label1.Text = "Username or Password does not exist! Try again";
+                }
+                else
+                {
+                    Session["user"] = loginTextBox.Text;
+                    Response.Redirect("WelcomePatient.aspx");
+                }
+            }
+            else if (Equals(str.Substring(0, 1),"e")==true)
+            {
+                c.cmd.CommandText = "Select * from Persons where EmployeeID='" + loginTextBox.Text + "' and pswd='" + passTextBox.Text + "'";
+                c.ds.Clear();
+                c.adp.Fill(c.ds, "vt");
+                if (c.ds.Tables["vt"].Rows.Count == 0)
+                {
+                    Label1.Text = "Username or Password does not exist! Try again";
+                }
+                else
+                {
+                    Session["user"] = loginTextBox.Text;
+                    Response.Redirect("WelcomeEmp.aspx");
+                }
+            }
+
+            else if (Equals(str.Substring(0, 1), "a") == true)
+            {
+                c.cmd.CommandText = "Select * from tableadmin where Adminid='" + loginTextBox.Text + "' and password='" + passTextBox.Text + "'";
+                c.ds.Clear();
+                c.adp.Fill(c.ds, "vt");
+                if (c.ds.Tables["vt"].Rows.Count == 0)
+                {
+                    Label1.Text = "Username or Password does not exist! Try again";
+                }
+                else
+                {
+                    Session["user"] = loginTextBox.Text;
+                    Response.Redirect("WelcomeEmp.aspx");
+                }
+            }
+
+            else
+            {
+                Label1.Text = "Invalid Userid";
+            }
+            
+
+            /*if (c.ds.Tables["vt"].Rows.Count > 0)
+            {
+                Class2.username = textBox1.Text.Trim().ToUpper();
+                //this.Hide();
+                MDIParent1 window = new MDIParent1();
+                window.Show();
+
+            }
+            else
+            {
+                label1.Text = "Login Failed";
+            }
+            */
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void passTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Home.aspx");
+        }
+    }
+}
