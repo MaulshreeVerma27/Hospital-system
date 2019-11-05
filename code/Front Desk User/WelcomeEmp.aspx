ea70875cde5dd4e04a98d7dd72be26d3dda3be4e
nagasaiteja <54856170+nagasaiteja@users.noreply.github.com>
2019-11-04 21:18:55 -0600

    Add files via upload

---
+++ code/Front Desk User/WelcomeEmp.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WelcomeEmp.aspx.cs" Inherits="HospitalSystem.WelcomeEmp" %>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+
+    .mybtn
+{  background-color: lightblue;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+ display: inline-block;
+  font-size: 12px;
+  margin: 4px 2px;
+  cursor: pointer;
+
+
+   //some more styles
+}
+body {
+  margin: 0;
+  font-family: Arial, Helvetica,sans-serif;
+}
+.topnav {
+  overflow: hidden;
+  background-color: lightblue;
+  width:100%;
+}
+.topnav tr {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 14px 10px;
+  text-decoration: none;
+  font-size: 12px;
+  width:100%
+}
+.topnav td:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav td.active {
+  background-color: #4CAF50;
+  color: black;
+}
+       
+    </style>
+</head>
+<body background="bg6.jpg">
+    <form id="form1" runat="server">   
+        <div>
+            <table class="topnav"  align="centre" runat="server">
+                <tr>
+     <td>
+         <asp:Button ID="Button1" class="mybtn" runat="server" Text="View appointments" OnClick="Button1_Click" /></td>
+                    <td>
+                        <asp:Button ID="Button2" class="mybtn" runat="server" Text="Upload Receipt" OnClick="Button2_Click" />    
+                    </td>
+                    <td>
+                        <asp:Button class="mybtn" ID="Button3" runat="server" Text="Home" OnClick="Button3_Click" />
+                        </td>
+                     <td>
+                        <asp:Button class="mybtn" ID="Button4" runat="server" Text="Logout" OnClick="Button4_Click" />
+                        </td>
+                        <td>
+                        <asp:Button class="mybtn" ID="Button5" runat="server" Text="Change Password" />
+                        </td>
+                    <td>Hello
+                        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+                    </td>
+                </tr>
+              
+            </table>
+            <table>
+                <tr>
+                    <td>
+                         <asp:Label ID="Label4" runat="server" Text="Select the patient Id" Visible="False"></asp:Label>
+                       <br />
+                       <asp:DropDownList ID="DropDownList1" runat="server" Visible="False" ></asp:DropDownList>
+                       <br />
+                       <asp:FileUpload ID="FileUpload1" runat="server" Visible="False" />
+                       <br />
+                       <asp:Button ID="Button7" runat="server" Text="Submit" OnClick="Button7_Click" Visible="False" />
+                       <br />
+                       <asp:Label ID="Label5" runat="server" Text=""></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ code/Front Desk User/WelcomeEmp.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HospitalSystem.App_Code;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HospitalSystem
+{
+    public partial class WelcomeEmp : System.Web.UI.Page
+    {
+        Class1 c = new Class1();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["user"] == null)
+                {
+                    Response.Redirect("Loginpage2.aspx");
+                }
+                else
+                {
+                    c.cmd.CommandText = "Select * from Persons where EmployeeID='" + Session["user"].ToString() + "'";
+                    c.ds.Clear();
+                    c.adp.Fill(c.ds, "vt");
+                    Label1.Text = c.ds.Tables["vt"].Rows[0]["EmployeeID"].ToString();
+
+
+                }
+
+            }
+            //Unit testing
+            WelcomeEmployeeUnittest we = new WelcomeEmployeeUnittest();
+                 
+            Response.Write(we.imagepath);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("appointmentrequests.aspx");
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("Loginpage2.aspx");
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("HomeP.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label4.Visible = true;
+            DropDownList1.Visible = true;
+            FileUpload1.Visible = true;
+            Button7.Visible = true;
+            string com = "select PatientID from Patient1";
+            SqlDataAdapter adpt = new SqlDataAdapter(com, c.con);
+
+            c.ds.Clear();
+            DataSet dt = new DataSet();
+            adpt.Fill(dt);
+            DropDownList1.DataSource = dt;
+            DropDownList1.DataBind();
+            DropDownList1.DataTextField = "PatientID";
+            DropDownList1.DataValueField = "PatientID";
+            DropDownList1.DataBind();
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                c.cmd.CommandText = "select * from Receipt";
+                c.ds.Clear();
+                c.adp.Fill(c.ds, "vt");
+                c.dr = c.ds.Tables["vt"].NewRow();
+                c.dr["EmployeeId"] = Session["user"].ToString();
+                c.dr["Day"] = DateTime.Now;
+                c.dr["PatientID"] = DropDownList1.SelectedValue;
+
+
+
+                if (FileUpload1.HasFile)
+                {
+                    string p = "~/images/" + FileUpload1.FileName;
+                    FileUpload1.SaveAs(Server.MapPath(p));
+                    c.dr["Image"] = p;
+
+                }
+                c.ds.Tables["vt"].Rows.Add(c.dr);
+
+                c.scb.DataAdapter = c.adp;
+                c.adp.Update(c.ds.Tables["vt"]);
+                //Label2.Text = str;
+                //Label3.Text = str1;
+                Label5.Text = "Receipt Uploaded successfully";
+            }
+
+            catch (Exception m)
+            {
+                Label1.Text = m.ToString();
+
+            }
+        }
+    }
+}
