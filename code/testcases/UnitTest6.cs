374192dec132b8dca0f8385c8abbec39082c59d8
yn0085 <44215351+yn0085@users.noreply.github.com>
2019-11-18 23:07:58 -0600

    Add files via upload

---
+++ code/testcases/UnitTest6.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HospitalSystem;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest6
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //when doctor id is not selected
+            Ratingunittest ob = new Ratingunittest();
+            Assert.AreEqual("", ob.docid);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //when doctor id is selected
+            Ratingunittest ob = new Ratingunittest();
+            Assert.AreEqual("doc1", ob.docid);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //when  patient id is null
+            Ratingunittest ob = new Ratingunittest();
+            Assert.AreEqual("doc1", ob.docid);
+            Assert.AreEqual("", ob.patientid);
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //when  patient id is selected
+            Ratingunittest ob = new Ratingunittest();
+            Assert.AreEqual("doc1", ob.docid);
+            Assert.AreEqual("pt10", ob.patientid);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //when  rating is greater than 5
+            Ratingunittest ob = new Ratingunittest();
+            Assert.AreEqual("doc1", ob.docid);
+            Assert.AreEqual("pt10", ob.patientid);
+            Assert.AreEqual(6, ob.rating);
+
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //when no rating is given
+            Ratingunittest ob = new Ratingunittest();
+            Assert.AreEqual("doc1", ob.docid);
+            Assert.AreEqual("pt10", ob.patientid);
+            Assert.AreEqual(0 , ob.rating);
+
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            //when  rating is greater than 5
+            Ratingunittest ob = new Ratingunittest();
+            Assert.AreEqual("doc1", ob.docid);
+            Assert.AreEqual("pt10", ob.patientid);
+            Assert.AreEqual(3, ob.rating);
+
+        }
+    }
+}
